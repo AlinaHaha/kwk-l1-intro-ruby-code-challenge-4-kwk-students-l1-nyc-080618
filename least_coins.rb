4b6ca5645dd0bfb7e76e137825d08853a5164d24
AlinaHaha <2ndalina@gmail.com>
2018-08-09 13:25:10 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,8 +1,8 @@
 def least_coins (189)
 
-(coins(189) = ({:quarters =>0, :dimes => 0, :nickels => 0, :pennies => 0}) 
+coins(189) = ({:quarters =>0, :dimes => 0, :nickels => 0, :pennies => 0} 
 
-puts least_coins
+
 
 end 
 
